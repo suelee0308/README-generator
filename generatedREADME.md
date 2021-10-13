000c5f2075d0dab3329f3fe0297e7995ca0e3117
Sue Lee <sue.lee0308@gmail.com>
2021-10-13 16:06:13 -0500

    links to licenses work correctly

--- generatedREADME.md
+++ generatedREADME.md
@@ -1,11 +1,13 @@
-# readme generator
+# 
   ## Description
-  generates a readme from the terminal
+  sdf
 
   ## License:
-  ![License: GNU General Public License v3.0](https://img.shields.io/badge/License-GNU%20General%20Public%20License%20v3.0-blue.svg)(https://opensource.org/licenses/GNU-General-Public-License-v3.0)
+  ![License: Eclipse Public License 2.0](https://img.shields.io/badge/License-Eclipse%20Public%20License%202.0-blue.svg)
 
-  This repo is licensed under the GNU General Public License v3.0 license.
+  https://opensource.org/licenses/MIT
+
+  This repo is licensed under the Eclipse Public License 2.0 license.
 
   ## Table of Contents
   * [Installation](#installation)
@@ -15,19 +17,19 @@
   * [Questions](#questions)
 
   ## Installation:
-  idk
+  sldkfjei
 
   ## Usage:
-  personal
+  soidfj
 
   ## Contributing
   undefined
 
   ## Tests
-      node index.js
+      slkd
 
   ## Questions:
   Please direct any questions you have about this repo to: 
-  - <https://github.com/suelee0308>
-  - <sue.lee0308@gmail.com>
+  - <https://github.com/woieurowie9>
+  - <039rwlkdfls@gmalsdkf>
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,8 +8,21 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  return (license === 'None' ? "" : 
-  `(https://opensource.org/licenses/${license.split(' ').join('-')})`);
+  return (license === 
+    'Apache 2.0' ? "https://opensource.org/licenses/Apache-2.0" :
+    'MIT' ? "https://opensource.org/licenses/MIT" : 
+    'GNU General Public License v3.0' ? "https://opensource.org/licenses/GPL-3.0" :
+    'BSD 2-Clause' ? "https://opensource.org/licenses/BSD-2-Clause" :
+    'BSD 3-Clause' ? "https://opensource.org/licenses/BSD-3-Clause" :
+    'Boost Software License 1.0' ? "https://choosealicense.com/licenses/bsl-1.0/" :
+    'Eclipse Public License 2.0' ? "https://opensource.org/licenses/EPL-2.0" :
+    'GNU Affero General Public License v3.0' ? "https://opensource.org/licenses/LGPL-3.0" :
+    'GNU General Public License v2.0' ? "https://opensource.org/licenses/LGPL-2.0" :
+    'GNU Lesser General Public License v2.1' ? "https://opensource.org/licenses/LGPL-2.1" :
+    'Mozilla Public License 2.0' ? "https://opensource.org/licenses/MPL-2.0" :
+    'The Unlicense' ? "https://choosealicense.com/licenses/unlicense/" :
+    ''
+    );
 }
 
 // TODO: Create a function that returns the license section of README
@@ -25,7 +38,8 @@ function generateMarkdown(data) {
   ${data.description}
 
   ## License:
-  ${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}\n
+  ${renderLicenseBadge(data.license)}\n
+  ${renderLicenseLink(data.license)}\n
   ${renderLicenseSection(data.license)}
 
   ## Table of Contents
